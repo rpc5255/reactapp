bfca205eeaf6cf5f8ca8b73ddf778b940b35a305
Rahul Chandran <rahul.chandran1024@gmail.com>
2018-12-17 17:42:36 -0500

    added background image

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import App from "./App";
+import './Login.css';
+import logo from "./logo.svg";
+
 
 
 class Login extends Component {
     render() {
         return (
             <div className="Login">
-                <header className="Login-header">
-                <h3>hello</h3>
-                </header>
+                <header className="Login-header"></header>
             </div>
         );
     }
