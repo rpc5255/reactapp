99f5945578f84dd09f25b2dd135ffbed7aff4481
David Luong <luongd5@student.lasalle.edu>
2018-12-17 18:13:00 -0500

    working on the body

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import App from "./App";
 
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-class Login extends Component {
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <div className="Login">
-                <header className="Login-header">
-                <h3>hello</h3>
-                </header>
-            </div>
+            <form className="Login" onSubmit={this.handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    Password:
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="Login" />
+                <input type="submit" value="Sign up Free"/>
+            </form>
         );
     }
 }
-
 export default Login;
