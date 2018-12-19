a025af73319c602d0b69385548527707bab54e68
Rahul Chandran <rahul.chandran1024@gmail.com>
2018-12-18 19:49:30 -0500

    finished login page

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import App from "./App";
 import './Login.css';
 import logo from "./logo.svg";
-
 
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: null ,
-            password: null };
+            email: null,
+            password: null
+        };
 
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -18,13 +18,14 @@ class Login extends Component {
     }
 
     handleEmailChange(event) {
-        this.setState({ email: event.target.value });
+        this.setState({email: event.target.value});
     }
 
     handlePasswordChange(event) {
-        this.setState({ password: event.target.value });
+        this.setState({password: event.target.value});
 
     }
+
     handleSubmit(event) {
         alert('An email was submitted: ' + this.state.email + " with password: " + this.state.password);
         event.preventDefault();
@@ -35,24 +36,23 @@ class Login extends Component {
             <div className="Login">
                 <div className="container">
                     <h4>Sign In</h4>
-
-
-            <form onSubmit={this.handleSubmit}>
-                <div className="email">
-                <label>Email</label>
-                    <input type="text" value={this.state.email} onChange={this.handleEmailChange} /><br />
-                    </div>
-                <div className="password">
-                <label>Password</label>
-                    <input type="password" value2={this.state.password} onChange={this.handlePasswordChange} /><br />
-                <input type="submit" value="Login!" /><br />
-                </div>
-                <text>
-                    Don't have an account?{"\n"}
-                    Sign up for one instantly!
-                </text>
-                <input type="submit" value="Sign-up!"/>
-            </form>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="email">
+                            <label>Email</label>
+                            <input type="text" value={this.state.email} onChange={this.handleEmailChange}/><br/>
+                        </div>
+                        <div className="password">
+                            <label>Password</label>
+                            <input type="password" value2={this.state.password}
+                                   onChange={this.handlePasswordChange}/><br/>
+                        </div>
+                        <input type="submit" value="Sign In"/><br/>
+                        <div className="signup">
+                            <text>
+                                Don't have an account? <a href="">Sign up now</a>
+                            </text>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
